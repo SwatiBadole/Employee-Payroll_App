a595db809a8f65ab5d766003d07a962ed1afe43f
SwatiBadole <swatibadole1997@gmail.com>
2022-06-05 19:25:32 -0700

    Added:on form submit papulate EmpPayrollData

--- js/payroll_form.js
+++ js/payroll_form.js
@@ -1,10 +1,8 @@
-<<<<<<< HEAD
 //UC8
-=======
->>>>>>> Day43_UC8_EventListenere_ON_salary
 const salary = document.querySelector('#salary');
 const output = document.querySelector('.salary-output');
 output.textContent = salary.value;
 salary.addEventListener('input', function() {
     output.textContent = salary.value;
 });
+
