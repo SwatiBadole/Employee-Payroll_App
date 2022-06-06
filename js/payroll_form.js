270cdaa88bc4ba478cd4f6f33b45870bbaf2aaae
SwatiBadole <swatibadole1997@gmail.com>
2022-06-06 16:04:55 -0700

    Added:on save create employee payroll object

--- js/payroll_form.js
+++ js/payroll_form.js
@@ -14,11 +14,53 @@ window.addEventListener('DOMContentLoaded', (event) => {
         nameError.textContent = e;
       }
     });
-
-const salary = document.querySelector('#salary');
-const output = document.querySelector('.salary-output');
-output.textContent = salary.value;
-salary.addEventListener('input', function() {
-    output.textContent = salary.value;
-    });
 });
+
+//Day44 UC3
+
+const save = () =>{
+  try{
+    let employeePayrollData = createEmployeePayroll();
+  }catch(e){
+    return;
+  }
+}
+const createEmployeePayroll = () => {
+  let employeePayrollData =new EmployeePayrollData();
+  try{
+    employeePayrollData.name = getInputValueByID('#name');
+  }catch(e){
+    setTextValue('.text-error',e);
+    throw e;
+  }
+  employeePayrollData.profileImage = getSelectedValues('[name=profile]').pop();
+  employeePayrollData.gender = getSelectedValues('[name = gender]').pop();
+  employeePayrollData.department = getSelectedValues('[name =department]');
+  employeePayrollData.salary = getSelectedValueById("#salary");
+  employeePayrollData.notes = getInputValueByID("#notes");
+
+  let date =getInputValueByID("#year") + " - " + getInputValueByID ("#month") + " - " + getInputValueByID("#day");
+  employeePayrollData.date = Date.parse(date);
+
+  alert(employeePayrollData.toString());
+  return employeePayrollData;
+}
+const getSelectedValues = (propertyValue) => {
+  let allItems =document.querySelectorAll(propertyValue);
+  let selItems = [];
+  allItems.forEach(item =>{
+    if(item.checked) selItems.push(item.value);
+  });
+  return selItems;
+};
+
+const getInputValueByID = (id) => {
+  let value = document.querySelector(id).value;
+  return value;
+};
+
+const getInputElementValue = (id) => {
+  let value =document.getElementById(id).value;
+  return value;
+};
+
